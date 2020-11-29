5cdfc7c4f80d5b07580d8935774404f76e53bb76
Piyush <piyushchauhan1004@gmail.com>
2020-11-30 04:01:46 +0530

    catkin workshop for basic ros learning

---
+++ build/ros_essentials_cpp-ros-noetic/CMakeFiles/ros_essentials_cpp_generate_messages_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "/home/optimus/catkin_ws/devel/share/common-lisp/ros/ros_essentials_cpp/msg/FibonacciAction.lisp"
+  "/home/optimus/catkin_ws/devel/share/common-lisp/ros/ros_essentials_cpp/msg/FibonacciActionFeedback.lisp"
+  "/home/optimus/catkin_ws/devel/share/common-lisp/ros/ros_essentials_cpp/msg/FibonacciActionGoal.lisp"
+  "/home/optimus/catkin_ws/devel/share/common-lisp/ros/ros_essentials_cpp/msg/FibonacciActionResult.lisp"
+  "/home/optimus/catkin_ws/devel/share/common-lisp/ros/ros_essentials_cpp/msg/FibonacciFeedback.lisp"
+  "/home/optimus/catkin_ws/devel/share/common-lisp/ros/ros_essentials_cpp/msg/FibonacciGoal.lisp"
+  "/home/optimus/catkin_ws/devel/share/common-lisp/ros/ros_essentials_cpp/msg/FibonacciResult.lisp"
+  "/home/optimus/catkin_ws/devel/share/common-lisp/ros/ros_essentials_cpp/msg/IoTSensor.lisp"
+  "/home/optimus/catkin_ws/devel/share/common-lisp/ros/ros_essentials_cpp/srv/AddTwoInts.lisp"
+  "CMakeFiles/ros_essentials_cpp_generate_messages_lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ros_essentials_cpp_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ devel/share/gennodejs/ros/ros_essentials_cpp/msg/_index.js
@@ -0,0 +1,22 @@
+
+"use strict";
+
+let IoTSensor = require('./IoTSensor.js');
+let FibonacciFeedback = require('./FibonacciFeedback.js');
+let FibonacciAction = require('./FibonacciAction.js');
+let FibonacciActionGoal = require('./FibonacciActionGoal.js');
+let FibonacciResult = require('./FibonacciResult.js');
+let FibonacciGoal = require('./FibonacciGoal.js');
+let FibonacciActionFeedback = require('./FibonacciActionFeedback.js');
+let FibonacciActionResult = require('./FibonacciActionResult.js');
+
+module.exports = {
+  IoTSensor: IoTSensor,
+  FibonacciFeedback: FibonacciFeedback,
+  FibonacciAction: FibonacciAction,
+  FibonacciActionGoal: FibonacciActionGoal,
+  FibonacciResult: FibonacciResult,
+  FibonacciGoal: FibonacciGoal,
+  FibonacciActionFeedback: FibonacciActionFeedback,
+  FibonacciActionResult: FibonacciActionResult,
+};
